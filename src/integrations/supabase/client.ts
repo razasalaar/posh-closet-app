905e67596993d70933582faf5791b4a557749a13
Raza Fazal <147403121+razasalaar@users.noreply.github.com>
2026-04-05 20:51:33 +0500

    Update Supabase URL and publishable key

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://uvmtzunnkpvghmrwiqmk.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2bXR6dW5ua3B2Z2htcndpcW1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzOTYzMDIsImV4cCI6MjA5MDk3MjMwMn0.Is4QtWeaw22-Q2YoMtn-rTjA-XmwxjuN58fSUew9zPA";
+const SUPABASE_URL = "https://bexzdveiveyxzvxtwmvg.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Ywl4jpklznoXC7GwEiiw5w_pbfO9-VS";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
