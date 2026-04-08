94c88cbfa65615c8768baa2ef67d67f03419320d
Raza Fazal <razafazal19@gmail.com>
2026-04-08 18:16:20 +0500

    fix logo

--- src/components/layout/WhatsAppButton.tsx
+++ src/components/layout/WhatsAppButton.tsx
@@ -1,15 +1,15 @@
-import { MessageCircle } from 'lucide-react';
+
 
 const WhatsAppButton = () => {
   return (
     <a
-      href="https://wa.me/923001234567"
+      href="https://wa.me/923166022412"
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-6 right-6 z-50 bg-[#25D366] hover:bg-[#20BD5A] text-primary-foreground rounded-full p-4 shadow-lg transition-transform hover:scale-110"
+      className="fixed bottom-6 right-6 z-50  text-primary-foreground rounded-full p-4  transition-transform hover:scale-110"
       aria-label="Chat on WhatsApp"
     >
-      <MessageCircle size={24} />
+      <img src="/w.png" alt="WhatsApp" className="w-12 h-12 object-contain" />
     </a>
   );
 };
